ed54b63987cb761a63ce7d86cb4184e613e02f28
Godwin Kamau <contactgodwinkamau@gmail.com>
2025-09-11 17:40:01 -0400

    added start and win functions

--- js/main.js
+++ js/main.js
@@ -1,49 +1,59 @@
+//----------address buttons and slots-------------//
+document.querySelector('#begin').addEventListener('click',begin)
 document.querySelector('#starp').addEventListener('click',spin)
 document.querySelector('#stort').addEventListener('click',spin2)
 document.querySelector('#begend').addEventListener('click',spin3)
+
 const slot = document.getElementById('slot1')
 const slot2 = document.getElementById('slot2')
 const slot3 = document.getElementById('slot3')
 
-let activia, activia2, activia3 = true
+const totalText = document.querySelector('#total')
+let total = 50
+totalText.innerText = total
+const message = document.querySelector('#message')
+const matchChecker = []
 
+
+//------------variables for checking interval--------------//
 let interID,interID2,interID3;
 
+function begin() {
+    const bid = Number(document.querySelector('#bid').value)
+//check for money input. If money is there and not past the total, start the game
+    if (total >= bid && bid !== 0) {
+        total -= bid
+        totalText.innerText = total
+        interID = setInterval(randomColor,500)        
+        interID2 = setInterval(randomColor2,500)
+        interID3 = setInterval(randomColor3,500)    
+    } else if (bid === 0 || total < bid) {
+        message.innerText = 'Nice Try, Bub.'
+    }
+}
+//---------------function to activate randomization---------//
 function spin() {
     console.log('clicked')
-    if (activia) {
-        interID = setInterval(randomColor,500)
-        activia = false
-    } else {
-        clearInterval(interID)
-        activia = true
-    }
+    clearInterval(interID)
+    interID = undefined
+    didYouWin()
 }
 
 function spin2() {
-    console.log('clicked')
-    if (activia2) {
-        interID2 = setInterval(randomColor2,500)
-        activia2 = false
-    } else {
-        clearInterval(interID2)
-        activia2 = true
-    }
+    console.log('clicked') 
+    clearInterval(interID2)
+    interID2 = undefined
+    didYouWin()
 }
 
 function spin3() {
-    console.log('clicked')
-    if (activia3) {
-        interID3 = setInterval(randomColor3,500)
-        activia3 = false
-    } else {
-        clearInterval(interID3)
-        activia3 = true
-    }
+    console.log('clicked')    
+    clearInterval(interID3)
+    interID3 = undefined
+    didYouWin()
 }
 
 function randomColor() {
-    console.log('ran')
     const picker = Math.ceil(Math.random()*3)
     if (picker === 1) {
         slot.className = 'blue'
@@ -52,10 +62,10 @@ function randomColor() {
     } else if (picker === 3) {
         slot.className = 'green'
     }
+    matchChecker[0] = picker
 }
 
 function randomColor2() {
-    console.log('ran')
     const picker = Math.ceil(Math.random()*3)
     if (picker === 1) {
         slot2.className = 'blue'
@@ -64,10 +74,10 @@ function randomColor2() {
     } else if (picker === 3) {
         slot2.className = 'green'
     }
+    matchChecker[1] = picker
 }
 
 function randomColor3() {
-    console.log('ran')
     const picker = Math.ceil(Math.random()*3)
     if (picker === 1) {
         slot3.className = 'blue'
@@ -76,5 +86,16 @@ function randomColor3() {
     } else if (picker === 3) {
         slot3.className = 'green'
     }
+    matchChecker[2] = picker
 }
 
+function didYouWin() {
+    if (interID || interID2 || interID3) {
+        return }
+    if (matchChecker[0]==matchChecker[1] && matchChecker[1]==matchChecker[2]) {
+        console.log('you did it!')
+    } else {
+        console.log('it\'s my money now')
+    }
+    
+}
